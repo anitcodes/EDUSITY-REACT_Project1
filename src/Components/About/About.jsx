092d193e3a9fd5f29b26ff8c6d670d14dcc8fe69
Anit Shrestha <anitshrestha101@gamil.com>
2025-06-20 19:51:44 +0545

    commited

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
 
 const About = () => {
   return (
     <div className='about'>
-        
+      
     </div>
   )
 }
