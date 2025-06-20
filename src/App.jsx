2e689816fcc5a43edd584dc5684c230aedb3f1c6
Anit Shrestha <anitshrestha101@gamil.com>
2025-06-20 19:34:17 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='OUR PROGRAM' title='WHAT WE OFFER?' />
+        <Programs />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+        
+    </div>
+  )
+}
+import './About.css'
+
+export default About
